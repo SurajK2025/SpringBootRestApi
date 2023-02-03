ddb5c183b8d4b6c1719464c0827c2ff60c942273
Suraj Khokrale <88073175+SurajK2025@users.noreply.github.com>
2023-02-03 23:05:15 +0530

    Delete spring_rest_api directory

--- spring_rest_api/src/main/java/com/app/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Note;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-
-}
