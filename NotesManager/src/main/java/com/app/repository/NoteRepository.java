ce8208e2aac413ace87cf622ff1466b1dab7e291
Suraj Khokrale <88073175+SurajK2025@users.noreply.github.com>
2023-02-07 07:56:46 +0530

    Update NoteRepository.java

--- NotesManager/src/main/java/com/app/repository/NoteRepository.java
+++ NotesManager/src/main/java/com/app/repository/NoteRepository.java
@@ -9,4 +9,7 @@ import com.app.pojos.User;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 	List<Note> findByUser(User user);
+	//@Query(value = "select new com.app.pojos.Employee(firstName,lastName) from Employee e where e.salary between ?1 and ?2")
+	//@Modifying
+	//@Query("update Employee e set e.salary=e.salary+?1 where e.department=?2 and e.joinDate < ?3")
 }
